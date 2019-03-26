1874499e9ab6f56202885fb165fc73b463f31be9
Dominic Montelongo and Heather Zurek <heather@>
2019-03-26 16:31:43 -0700

    trying to simplify js

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  $('.clickable').click(function() {
+    $(this).next.toggle();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $(".clickable0").click(function() {
+//     $("#initially-hidden0").removeClass();
+//     $("#initially-hidden0").fadeToggle();
+//   });
+//   $(".clickable1").click(function() {
+//     $("#initially-hidden1").removeClass();
+//     $("#initially-hidden1").toggle();
+//   });
+//   $(".clickable1").click(function() {
+//     $("#initially-hidden2").removeClass();
+//     $("#initially-hidden2").toggle();
+//   });
+//   $(".clickable1").click(function() {
+//     $("#initially-hidden3").removeClass();
+//     $("#initially-hidden3").toggle();
+//   });
+//   $(".clickable1").click(function() {
+//     $("#initially-hidden4").removeClass();
+//     $("#initially-hidden4").toggle();
+//   });
+//   $(".clickable1").click(function() {
+//     $("#initially-hidden5").removeClass();
+//     $("#initially-hidden5").toggle();
+//   });
+//   $(".clickable6").click(function() {
+//     $("#initially-hidden6").removeClass();
+//     $("#initially-hidden6").toggle();
+//   });
+//   $(".clickable7").click(function() {
+//     $("#initially-hidden7").removeClass();
+//     $("#initially-hidden7").toggle();
+//   });
+//   $(".clickable8").click(function() {
+//     $("#initially-hidden8").removeClass();
+//     $("#initially-hidden8").toggle();
+//   });
+//   $(".clickable9").click(function() {
+//     $("#initially-hidden9").removeClass();
+//     $("#initially-hidden9").toggle();
+//   });
+//   $(".clickable10").click(function() {
+//     $("#initially-hidden10").removeClass();
+//     $("#initially-hidden10").toggle();
+//   });
+// });
